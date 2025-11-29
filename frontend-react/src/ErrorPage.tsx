2b339f8fd8e7248bb01c6892e2e2976188802675
Szymon <szymongryz@interia.pl>
2025-11-29 16:20:09 +0100

    init structure created

---
+++ frontend-react/src/ErrorPage.tsx
@@ -0,0 +1,9 @@
+function ErrorPage() {
+  return (
+    <div className="flex justify-center items-center min-h-dvh text-5xl">
+      404 Not Found
+    </div>
+  );
+}
+
+export default ErrorPage;
---
+++ frontend-react/src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import ErrorPage from "./ErrorPage.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+);
